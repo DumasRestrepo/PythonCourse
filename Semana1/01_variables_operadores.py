a718b3613a903e135512ff38896e8b99f8e9e12b
ChristianJaimes <christiandja@gmail.com>
2025-04-26 12:01:50 -0500

    Semana 1 Sabado 26

---
+++ Semana1/01_variables_operadores.py
@@ -0,0 +1,27 @@
+numero_1 = 4
+numero_2 = 7
+
+restultado = numero_2 / numero_1
+modulo =numero_2 % numero_1
+div = numero_2 // numero_1
+multi = numero_1 * numero_2
+potencia = numero_1 ** numero_2
+
+print(f"restultado de la div {restultado }")
+print(f"restultado de la div {modulo}")
+print(f"restultado de la div {div}")
+print(f"restultado de la multiplicacion {multi}")
+print(f"restultado de la potencia {potencia}")
+
+
+#operadores logicos And or Not
+
+expresion_1 = True
+expresion_2 = False
+r = expresion_1 and expresion_2
+o = expresion_1 or expresion_2
+n = not expresion_2
+
+print(f"{expresion_1} AND {expresion_2} es: {r}")
+print(f"{expresion_1} OR {expresion_2} es: {o}")
+print(f" NOT {expresion_2}  es: {n}")
