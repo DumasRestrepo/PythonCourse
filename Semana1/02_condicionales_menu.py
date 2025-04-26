625294d212cdcabed9901989bc67aaf6a3472244
ChristianJaimes <christiandja@gmail.com>
2025-04-26 14:42:16 -0500

    condicional simple

---
+++ Semana1/02_condicionales_menu.py
@@ -0,0 +1,20 @@
+"""condionales_menu.py
+Comparar dos numeros enteros
+"""
+
+# Declaracion de variables
+numero_1 = 0
+numero_2 = 0
+resultado = 0
+
+# entrada de datos
+numero_1 = int(input("Ingrese el primer numero: "))
+numero_2 = int(input("Ingrese el segundo numero: "))
+# proceamiento y salida}
+if numero_1 == numero_2:
+    print(f"Los numeros son iguales {numero_1} == {numero_2}")
+else:
+    print(f"Los numeros son diferentes {numero_1} != {numero_2}")
+
+
+
