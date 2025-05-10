2ba3d5521d011ffb26baab4cc57bae19f0234909
ChristianJaimes <christiandja@gmail.com>
2025-05-10 09:03:08 -0500

    Repaso semana 1 y 2

---
+++ Semana3/00_repaso.py
@@ -0,0 +1,58 @@
+#Definicion de vairables y tipos de datos
+
+libreria = [] # ID: 10002123123 
+libros_romance = "" #buena practica en python
+librosRomance = "" # Mala practica en python pero buena en otros lenguajes
+#0libreria = "" #No es posible 
+__ibro3= ""
+
+# Typos de datos
+numero_entero = 3 # -1231231231232, 12324123123, 0
+numero_flotante_1 = 5.7 # Tambien pueden ser negativos
+numero_flotatnte_2 = 4e3 # 4 *10**3  o  4 mil
+
+# Bool
+is_client = True
+is_adult = False
+
+# String
+
+msj_1 = "3"
+msj_2 = 'Hola \nSoy\nChistian'
+msj_3 = """soy 
+        christian
+       """
+#Listas
+
+lista_1 = []
+lista_2 = [3,4,5,6]
+lista_3 = [3,"hola",4,8.9]
+
+#Diccionarios
+dictionary ={}
+dictionary_1 = {'nombre':'Mighel'} # Estructura de llave : valor
+dictionary_2 = {1:"mighel"}
+
+# Tupla
+descuentos = (4,4,1)
+# None = null
+texto = '' # Str vacio # carrera 5 # 34-09
+texto_1 = None # Avenida siempre viva 123 # NO apunta a ninguna direccipn de memoria
+
+print(numero_entero)
+print('la variable numero_entero contiene ', numero_entero)
+print(f'La variable numero_flotante contiene: {numero_flotante_1}')
+print(f'La variable msj_1 = {msj_1}, es un:{type(msj_1)}')
+print("""
+hola
+      soy
+      christian
+      """)
+
+
+
+
+
+
+
+
